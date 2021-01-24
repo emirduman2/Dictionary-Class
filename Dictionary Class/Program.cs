6f811c1d936b5bba81c108f2fb11bed993fd6dfb
Emir Can Ünlütaş <s4.icin1@hotmail.com>
2021-01-25 00:59:31 +0300

    Proje dosyası ekle.

---
+++ Dictionary Class/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary_Class
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> Ogrenci = new Dictionary<int, string>();
+            Ogrenci.Add(134, "Tolga Demirer");
+            Ogrenci.Add(158, "Ümit Özkan");
+            Ogrenci.Add(115, "Kadir Aydemir");
+            Ogrenci.Add(174, "Cemal Çiftçi");
+            Console.Write("Öğrenci No Giriniz:");
+            int No = int.Parse(Console.ReadLine());
+            try
+            {
+                Console.WriteLine(Ogrenci[No]);
+            }
+            catch
+            {
+                Console.WriteLine("Öğrenci Bulunamadı.");
+            }
+        }
+    }
+}
